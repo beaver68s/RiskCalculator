dada33bedfb09f9f8b20a52f535c4c1868759c27
Yuriy <yuriy@Beaver-Pro.local>
2025-12-06 22:46:10 +0300

    Add RiskCalculator files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import plotly.exfpress as px
+import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from typing import Dict, List, Tuple
